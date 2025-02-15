deb6b01f89d1e01430ac697bbb29c17f5a7097b1
Sabrina Yuan <syuan22@students.cs.ubc.ca>
2025-02-15 14:59:16 -0800

    Added test for Food class

--- src/test/model/FoodTest.java
+++ src/test/model/FoodTest.java
@@ -16,29 +16,43 @@ public class FoodTest {
     }
 
     @Test
-    void testConstructor() {
-        assertEquals("Chicken", testFoodBacon.getName());
-        assertEquals(37, testFoodBacon.getProteinCountPerHundredGrams());
+    public void testConstructor() {
+        assertTrue(testFoodBacon != null);
+        assertTrue(testFoodShrimp != null);
     }
 
     @Test
-    void testGetName() {
-
+    public void testGetName() {
+        assertTrue(testFoodBacon.getName() != null);
+        assertEquals(testFoodBacon.getName(), "Bacon");
+        assertTrue(testFoodBacon.getName() != null);
     }
 
     @Test
-    void testGetProteinCountPerHundredGrams() {
-
+    public void testGetProteinCountPerHundredGrams() {
+        assertTrue(testFoodBacon.getName() != null);
+        assertTrue(testFoodBacon.getProteinCountPerHundredGrams() != 0);
+        assertEquals(testFoodBacon.getProteinCountPerHundredGrams(), 37);
+        assertTrue(testFoodBacon.getName() != null);
     }
 
     @Test
-    void testSetName() {
-
+    public void testSetName() {
+        assertTrue(testFoodShrimp != null);
+        assertEquals(testFoodShrimp.getName(), "Shrimp");
+        assertEquals(testFoodShrimp.getProteinCountPerHundredGrams(), 20);
+        testFoodShrimp.setName("Chicken");
+        assertTrue(testFoodShrimp != null);
+        assertEquals(testFoodShrimp.getName(), "Chicken");
+        assertEquals(testFoodShrimp.getProteinCountPerHundredGrams(), 20);
     }
 
     @Test
-    void testSetProteinCountPerHundredGrams() {
-        
+    public void testSetProteinCountPerHundredGrams() {
+        assertTrue(testFoodBacon != null);
+        assertEquals(testFoodBacon.getProteinCountPerHundredGrams(), 37);
+        testFoodBacon.setProteinCountPerHundredGrams(30);
+        assertTrue(testFoodBacon != null);
+        assertEquals(testFoodBacon.getProteinCountPerHundredGrams(), 30);
     }
-
 }
